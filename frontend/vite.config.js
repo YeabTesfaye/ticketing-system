170059156c498dca6436642adf3689cf3dad8a4b
yeabTesfaye <tesfayeyeabsra446@gmail.com>
2025-03-10 17:36:33 +0300

    chore: Update Render URL for backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://ticketing-system-server-3.onrender.com',
         changeOrigin: true,
       },
     },
