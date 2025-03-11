f93a76f4e81dc5f97b416ab361291b05aa9209a4
yeabTesfaye <tesfayeyeabsra446@gmail.com>
2025-03-11 09:03:55 +0300

    feat: serve React frontend in production using Express

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { connectDB } from './utils/db.js';
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 import { ticketRouter } from './route/ticketRoutes.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 connectDB();
 const PORT = process.env.PORT || 5000;
@@ -15,6 +16,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/api/users', UserRouter);
 app.use('/api/tickets', ticketRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve(); 
+  app.use(express.static(path.join(__dirname, '../frontend/dist'))); 
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html')),
+  );
+} else {
+  app.get('/', (req, res) => res.send('Server is ready!'));
+}
 app.use(notFound);
 app.use(errorHandler);
 app.listen(PORT, () => {
